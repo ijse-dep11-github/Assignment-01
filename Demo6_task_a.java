b2aa3188ff77fca36945a68964ef0808561df7ce
Viraji Sesiri <viraji.sesiri@gmail.com>
2023-07-31 09:04:02 +0530

    Complete Task 6-B

--- Demo6_task_a.java
+++ Demo6_task_a.java
@@ -1,5 +1,21 @@
 public class Demo6_task_a {
     public static void main(String[] args) {
+
+        String num1 = "5";
+        String num2 = "0";
+        String syn1 ="[";
+        String syn2 = "]";
+
+        
+
+        final String B1 = "\033[41m";
+        final String B2 = "\033[42m";
+        final String R = "\033[0m";
+
+        final String F = "\033[30;1m";
+
+
+        System.out.printf("%5$s%1$s%2$7s%8$s%6$s%7$s%3$s%%%4$7s%8$s\n",syn1,num1,num2,syn2,B1,B2,F,R);
         
     }
     
---
+++ Demo6_task_b.java
@@ -0,0 +1,6 @@
+public class Demo6_task_b {
+    public static void main(String[] args) {
+        
+    }
+    
+}
